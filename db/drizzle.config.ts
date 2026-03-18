78a2a81ee02d7e2e7864245fa8b4f8cd637a0e66
Anirban Das <allmail.anirban@gmail.com>
2026-03-18 22:05:04 +0530

    updated drizzle config

--- db/drizzle.config.ts
+++ db/drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 import { env } from "./config/index.js";
 
 export default {
-  schema: "./schema.ts",
+  schema: "./schema/index.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
